8b19fff9faa4a570ce0e879c5fbdef06c20b4f8a
Yevhen Basistyi <psik726@gmail.com>
2024-09-27 17:18:04 +0200

    Fixed pod localization issue, removed cached file

--- TemplateSwiftUIApp/App/AppDelegate.swift
+++ TemplateSwiftUIApp/App/AppDelegate.swift
@@ -6,19 +6,15 @@
 //
 
 import UIKit
-import Resolver
-import RevenueCat
 
 @main
 class AppDelegate: NSObject, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-
         setupMain()
         setupSettings()
         setupPaywall()
-
         return true
     }
 
